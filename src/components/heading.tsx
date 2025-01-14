c4ff91f732a9e199efb0219dea5808f23c4a2be5
ethanmidgley <ethan.c.midgley@gmail.com>
2025-01-14 20:26:17 +0000

    Updated components

---
+++ src/components/heading.tsx
@@ -0,0 +1,34 @@
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+
+type size = "xs" | "sm" | "md" | "lg" | "xl";
+
+type HeadingProps = {
+  size?: size;
+} & DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>;
+
+const size_to_font_size = (s: size) => {
+  const mappings: Record<size, string> = {
+    xs: "text-base",
+    sm: "text-lg",
+    md: "text-xl",
+    lg: "text-2xl",
+    xl: "text-4xl",
+  };
+  return mappings[s] + " ";
+};
+
+export default function Heading({
+  children,
+  className,
+  size = "lg",
+  ...props
+}: HeadingProps) {
+  return (
+    <h1
+      {...props}
+      className={"text-purple-400 " + size_to_font_size(size) + className}
+    >
+      {children}
+    </h1>
+  );
+}
