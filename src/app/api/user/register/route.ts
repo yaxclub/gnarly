3509e0190ffa69b25e4429deaae52e8551245832
Calum Cardownie <calum.cardownie@gmail.com>
2025-01-28 00:56:59 +0000

    register route created by ethan

--- src/app/api/user/register/route.ts
+++ src/app/api/user/register/route.ts
@@ -2,10 +2,12 @@ import db from "@/db/db";
 import { users } from "@/db/schema/user";
 import { NextResponse } from "next/server";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, or } from "drizzle-orm";
 import * as argon from "argon2";
+import {user_info}  from "@/db/schema/user_info";
 
 const RegisterRequest = z.object({
+  username: z.string(),
   email: z.string(),
   password: z.string(),
   dob: z.string().date(),
@@ -14,34 +16,39 @@ const RegisterRequest = z.object({
 });
 
 export const POST = async (req: Request): Promise<NextResponse> => {
+
   const { success, data } = RegisterRequest.safeParse(await req.json());
 
   if (!success) {
-    return NextResponse.json({ message: "you idiot" });
+    return NextResponse.json({ message: "NI*KINPOOP" });
   }
 
   const u = await db
     .select()
     .from(users)
-    .where(eq(users.email, data.email))
+    .where(or(eq(users.email, data.email), eq(users.username, data.username)))
     .limit(1);
 
   if (u.length > 0) {
-    return NextResponse.json({ message: "email already has an account" });
+    return NextResponse.json({ message: "email/username already has an account" });
   }
 
   const hashed_password = await argon.hash(data.password);
 
   const result = await db.insert(users).values({
-    email: data.email,
+    username: data.username,
+    email: data.email, 
+    password: hashed_password,
+  }).$returningId();
+
+  await db.insert(user_info).values({
+    user_id: result[0].id,
     dob: new Date(data.dob),
     forename: data.forename,
     surname: data.surname,
-    password: hashed_password,
     bio: "",
     picture: "",
   });
-  console.log(result);
 
   return NextResponse.json(data);
 };
